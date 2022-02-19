8b3742434fe282d232430a65492bfad0fbc0a10c
Akarsh Barar <barar.akarsh8@gmail.com>
2022-02-20 00:40:59 +0530

    Interacting withing contracts

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -14,6 +14,28 @@ contract StorageFactory {
     function sfStore(uint256 _simpleStorageIndex, uint256 _simpeStoragenumber) public {
         //Address
         //ABI
+        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpeStoragenumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrive();
+    }
+}// SPDX-Licence-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory {
+
+    SimpleStorage[] public simpleStorageArray;
+    function createSimpleStoreageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpeStoragenumber) public {
+        //Address
+        //ABI
         SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
         simpleStorage.store(_simpeStoragenumber);
     }
