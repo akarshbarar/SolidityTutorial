e0260e19fe6151abf9a99686702b1d405e040e52
Akarsh Barar <barar.akarsh8@gmail.com>
2022-02-20 22:29:21 +0530

    Added payable function to transfer funds

--- FundMe.sol
+++ FundMe.sol
@@ -3,7 +3,10 @@ pragma solidity >=0.6.6 <0.9.0;
 
 contract FundMe {
 
+
+    mapping(address => uint256) public addresToAmountFunded;
+
     function fund() public payable {
-        
+        addresToAmountFunded[msg.sender] += msg.value;
     }
 }
